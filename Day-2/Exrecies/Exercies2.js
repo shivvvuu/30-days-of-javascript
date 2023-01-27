3650fd43fc1a9ffb1de1e180194a2d83097f7444
shivam <jhodshivam5122@gmail.com>
2023-01-27 23:58:23 +0530

    Day2EX2

---
+++ Day-2/Exrecies/Exercies2.js
@@ -0,0 +1,12 @@
+// Using console.log() print out the following statement: The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
+console.log(' The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another.');
+//Using console.log() print out the following quote by Mother Teresa:
+console.log("\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\"")
+//Check if typeof '10' is exactly equal to 10. If not make it exactly equal
+console.log(typeof(10) == 10);
+//Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10
+console.log(parseFloat('9.8') == 10);
+console.log(Math.ceil(parseFloat('9.8')) == 10)
+//Check if 'on' is found in both python and jargon
+let py = 'python'
+console.log(py.includes('on'));
